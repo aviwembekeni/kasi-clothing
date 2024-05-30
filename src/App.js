8ae5f067417fae17737c463439cb8ddfb974327f
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-05-30 00:17:18 -0700

    implemtanted pages navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./routes/home/home.component";
-
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component.jsx";
+import Shop from "./routes/shop/shop.component";
+import SignIn from "./routes/sign-in/sign-in.component.jsx";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
     </Routes>
   );
 };
