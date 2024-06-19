5458316c9bffe24f5efa6c92da6b81c4502d42e1
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-06-19 15:04:16 +0200

    created checkout page along with checkout items and checkout cart functionality

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,32 +1,43 @@
 import { useContext } from "react";
 
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 import { CartContext  } from "../../contexts/cart.context";
 
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-    const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext);
+    const { cartItems, cartTotal } = useContext(CartContext);
 
     return (
-        <div>
-            <h1>I am the checkout page </h1>
-            <div>
+            <div className="checkout-container">
+                <div className="checkout-header">
+                    <div className="header-block">
+                        <span>Product</span>
+                    </div>
+                     <div className="header-block">
+                     <span>Description</span>
+
+                    </div>
+                    <div className="header-block">
+                    <span>Quantity</span>
+
+                    </div>
+                    <div className="header-block">
+                    <span>Price</span>
+
+                    </div>
+                    <div className="header-block">
+                    <span>Remove</span>
+
+                    </div>
+                </div>
                 {
-                    cartItems.map((cartItem) => {
-                        const { id, name, quantity } = cartItem;
-                        return (
-                            <div key={id}>
-                                <h2>{name}</h2>
-                                <span>{quantity}</span>
-                                
-                                <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
-                                <br />
-                                <span onClick={() => addItemToCart(cartItem)}>increment</span>
-                            </div>
-                        )
-                    })
+                    cartItems.map((cartItem) => 
+                            <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+                    )
                 }
-            </div>
+                <span className="total">Total:  $ {cartTotal}</span>
         </div>
     )
 }
