9e0afa894b636473300eeef607f1837dfeeaa635
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-06-21 13:30:46 +0200

    added categories preview route

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Home from "./routes/home/home.component.jsx";
 import Shop from "./routes/shop/shop.component";
 import Authentication from "./routes/authentication/authentication.component.jsx";
 import Checkout from "./routes/checkout/checkout.component.jsx";
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
-        <Route path= "checkout" element={<Checkout/>} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
