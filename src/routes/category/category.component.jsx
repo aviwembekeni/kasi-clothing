6ac661c7791ac4f2a96ba5d6ca418127e1202174
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-07-10 13:33:39 +0200

    moved category to routes

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 
-import ProductCard from "../product-card/product-card.components";
+import ProductCard from "../../components/product-card/product-card.components";
 
 import { CategoriesContext } from "../../contexts/categories.context";
 
