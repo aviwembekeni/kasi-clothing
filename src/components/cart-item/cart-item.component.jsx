baec1af7614096ec9760bd6332234658d403c688
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-07-10 13:30:43 +0200

    changed cart-item and category styles from scss to styled components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,19 @@
-import "./cart-item.styles.scss";
+import { ItemDetails, Name, CartItemContainer } from "./cart-item.styles";
 
 const CartItem = ({ cartItem }) => {
-    const {name, quantity, imageUrl, price} = cartItem;
+  const { name, quantity, imageUrl, price } = cartItem;
 
-    return (
-        <div className="cart-item-container">
-            <img src={imageUrl} alt={name}/>
-            <div className="item-details">
-                <span className="name">{name}</span>
-                <span className="price">{quantity} X ${price}</span>
-            </div>
-        </div>
-    )
-}
+  return (
+    <CartItemContainer>
+      <img src={imageUrl} alt={name} />
+      <ItemDetails>
+        <Name>{name}</Name>
+        <span className="price">
+          {quantity} X ${price}
+        </span>
+      </ItemDetails>
+    </CartItemContainer>
+  );
+};
 
 export default CartItem;
