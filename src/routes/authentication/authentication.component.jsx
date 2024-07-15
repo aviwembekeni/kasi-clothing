a641ec15443ea3805da6e43b735833efd7c59341
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-07-15 11:27:43 +0200

    Changed Authentication route component style from scss to styled-components

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,14 +1,15 @@
-import SignUpForm from "../../components/sign-up-form/sign-up-form.component.jsx";
-import SignInForm from "../../components/sign-in-form/sign-in-form.component.jsx";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-import "./authentication.styles.scss";
-const Authencation = () => {
+import { AuthenticationContainer } from "./authentication.styles";
+
+const Authentication = () => {
   return (
-    <div className="authentication-container">
+    <AuthenticationContainer>
       <SignInForm />
       <SignUpForm />
-    </div>
+    </AuthenticationContainer>
   );
 };
 
-export default Authencation;
+export default Authentication;
