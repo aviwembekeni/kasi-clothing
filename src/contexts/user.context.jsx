9bd4d6009fbf567cb68c994aa4e9e431d37d9d3c
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-12-05 10:40:05 +0200

    Modified user context to use createAction helper function inside dspatch function

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useReducer } from "react";
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 //as the actual value you want to access
 export const UserContext = createContext({
@@ -37,7 +38,9 @@ export const UserProvider = ({children}) => {
   const { currentUser } = state; 
 
   const setCurrentUser = (user) => {
-    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+    dispatch(
+      createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)
+    )
   }
 
     // instead of using useState here, we use a reducer
